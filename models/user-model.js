df1ac4ff461b082d66ccda6de42a546eb1cebbc6
rodpru <rgp.prt@gmail.com>
2020-12-07 10:47:50 +0000

    profile

--- models/user-model.js
+++ models/user-model.js
@@ -5,6 +5,7 @@ const userSchema = new Schema(
   {
     username: String,
     password: String,
+    favourites: [String],
     googleId: String,
   },
   {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const User = require("../models/user-model");
+const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+router.get("/profile", (req, res, next) => {
+  const username = req.body.username;
+  User.findOne(username).then((user) => {
+    res.json(user);
+  });
 });
 
 module.exports = router;
