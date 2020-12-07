e030774e2bd0c1031c89af41d42ceb58e24af9c6
djnokin <nokindj@gmail.com>
2020-12-07 12:52:09 +0000

    done

--- models/user-model.js
+++ models/user-model.js
@@ -5,7 +5,8 @@ const userSchema = new Schema(
   {
     username: String,
     password: String,
-    favourites: [String],
+    favoriteNurseries: [String],
+    favoriteKindergartens: [String],
     googleId: String,
   },
   {
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,30 @@
 const express = require("express");
 const User = require("../models/user-model");
 const router = express.Router();
+const School = require("../models/school-model");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
-router.get("/profile", (req, res, next) => {
-  const username = req.body.username;
-  User.findOne(username).then((user) => {
-    res.json(user);
+router.get("/profile/:id", (req, res, next) => {
+  const id = req.params.id;
+  User.findById(id).then((user) => {
+    return School.find({ _id: user.favoriteNurseries }).then((response) => {
+      console.log(response);
+      res.json(response);
+    });
+  });
+});
+
+router.post("/profile", (req, res) => {
+  const username = req.body.user;
+  const favorite = req.body.id;
+  console.log(username, favorite);
+  User.findByIdAndUpdate(username, {
+    $push: { favoriteNurseries: favorite },
+  }).then((response) => {
+    res.json(response);
   });
 });
 
